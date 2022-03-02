934918c5a9f7e8908d382e11551373866472adf0
ayushi25jaiswal <ayushijaiswal2507@gmail.com>
2022-03-02 13:14:22 +0530

    Initial Commit

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -14,6 +14,39 @@ def todolist(request):
         all_task = TaskList.objects.all
         return render(request, 'todolist.html', {'all_task': all_task})
 
+def delete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.delete()
+
+    return redirect('todolist')
+
+def edit_task(request, task_id):
+    if request.method == "POST":
+        task = TaskList.objects.get(pk=task_id)
+        form = TaskForm(request.POST or None, instance= task)
+        if form.is_valid():
+            form.save()
+        messages.success(request, ("Task Edited"))
+        return redirect('todolist')
+
+    else:
+        task_obj = TaskList.objects.get(pk=task_id)
+        return render(request, 'edit.html', {'task_obj': task_obj})
+
+def complete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = True
+    task.save()
+
+    return redirect('todolist')
+
+def pending_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = False
+    task.save()
+
+    return redirect('todolist')
+
 def contact(request):
     return render(request, 'contact.html', {'contact_text': "Welcome to contact"})
 
